6a7ce7c953d4b9c26d5db86be4c9860f07a7f4c6
Paula Patrice Fudolig <pfudolig@lxplus7107.cern.ch>
2022-07-12 13:18:30 +0200

    Plotting by Stream Amount

--- analyze_serial.py
+++ analyze_serial.py
@@ -18,6 +18,7 @@ def storeByEvent(maxEvents):
         output = p.communicate()
         mystring = str(output)
         parts = mystring.split(' ')
+        nval = float(parts[10])
         nevents.append(float(parts[10]))
         time.append(float(parts[13]))
         throughput.append(float(parts[16]))
@@ -27,50 +28,56 @@ def storeByEvent(maxEvents):
     #print(df)
     return(df)
 
-#ten_events = storeData(10)
-#hundred_events = storeData(100)
-#five_hundred_events = storeData(500)
+#print(storeByEvent(10))
 
 
 def storeByStream(nStreams,maxEvents):
     '''Loop over inputted number of events to record processing time and throughput of serial processing'''
     time = []
     throughput = []
-    streams = nStreams
+    streams = []
 
     for i in range(1,nStreams+1): #exclude 0
         cmd = "./serial --numberOfStreams " + str(i) + ' --maxEvents ' + str(maxEvents)
-        print(cmd)
-        #p = Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-        #output = p.communicate()
-        #mystring = str(output)
-        #parts = mystring.split(' ')
-        #nevents.append(float(parts[10]))
-        #time.append(float(parts[13]))
-        #throughput.append(float(parts[16]))
+        p = Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+        output = p.communicate()
+        mystring = str(output)
+        parts = mystring.split(' ')
+        time.append(float(parts[13]))
+        throughput.append(float(parts[16]))
+        streams.append(float(parts[5]))
 
-    #d = {'nevents': nevents, 'time': time, 'throughput': throughput}
-    #df = pd.DataFrame(data=d)
+    d = {'nStreams': streams, 'time': time, 'throughput': throughput}
+    df = pd.DataFrame(data=d)
     #print(df)
-    #return(df)
+    return(df)
 
-print(storeByStream(10,100))
+ten_streams = storeByStream(10,10)
+fthousand_events = storeByStream(20,5000)
 
 
 def plotDF(dataframe):
     '''Plot serial processing data'''
-    df_nevents = dataframe['# of Streams']
+    df_streams = dataframe['nStreams']
     df_time = dataframe['time']
     df_throughput = dataframe['throughput']
-    plt.bar(df_nevents,df_time)
-    plt.xticks()
-    plt.xlabel('Amount of Events Ran Concurrently')
-    plt.ylabel('Processing Time of Events (s)') 
-    plt.title('Processing Time for ' + str(max(df_nevents)) + ' Events Ran (Serially with 1 Thread)') 
-    plt.show() 
-    plt.savefig('/afs/cern.ch/user/p/pfudolig/pixeltrack-standalone/time_v_10events.png')
 
-#plotDF(ten_events)
+    plt.bar(df_streams,df_time)
+    plt.xticks()
+    plt.xlabel('Amount of Streams')
+    plt.ylabel('Time of 5000 Events (s)') 
+    plt.title('Processing Time for ' + str(max(df_streams)) + ' Streams Ran Concurrently (Serial)') 
+
+    #plt.bar(df_streams,df_throughput)
+    #plt.xticks()
+    #plt.xlabel('Amount of Streams')
+    #plt.ylabel('Throughput (events/s)') 
+    #plt.title('Throughput of ' + str(max(df_streams)) + ' Streams Ran Concurrently (Serial)') 
+
+    plt.show() 
+    #plt.savefig('/afs/cern.ch/user/p/pfudolig/pixeltrack-standalone/time_v_20s_5000e.png')
+
+plotDF(fthousand_events)
 
 
 
